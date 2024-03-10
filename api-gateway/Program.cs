b8ac22c296551a881f3eb5474a71e71f49fb13ab
Redtius <redtius@gmail.com>
2024-03-10 16:04:04 +0000

    api-gateway fix

--- api-gateway/Program.cs
+++ api-gateway/Program.cs
@@ -3,14 +3,15 @@ using Ocelot.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json",optional: false, reloadOnChange: true)
+    .AddJsonFile("Ocelot.json",optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 builder.Services.AddSignalR();
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
-await app.UseOcelot();
+app.UseRouting();
+app.UseOcelot();
 
 
 app.Run();
