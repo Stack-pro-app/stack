b2826fd3dda63dfd03623b210b9d4396c2f7cfa8
Redtius <redtius@gmail.com>
2024-05-09 13:08:55 +0100

    commit

--- Front/src/app/features/pages/Welcome/welcome/welcome.component.ts
+++ Front/src/app/features/pages/Welcome/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, Type } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 import { RouterLink } from '@angular/router';
 import { TypeWriterDirective } from '../../../../core/Directives/TypeWriterDirective';
@@ -10,6 +10,10 @@ import { TypeWriterDirective } from '../../../../core/Directives/TypeWriterDirec
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  isLogged:boolean = false;
+  ngOnInit(): void {
+  this.isLogged = localStorage.getItem('token') ? true : false;
+  }
 
 }
