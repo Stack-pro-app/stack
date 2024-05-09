907b648d27d9e66af65e557a0a4d1a0492bc93b9
Redtius <redtius@gmail.com>
2024-05-09 20:39:56 +0100

    Update welcome.component.ts

--- Front/src/app/features/pages/Welcome/welcome/welcome.component.ts
+++ Front/src/app/features/pages/Welcome/welcome/welcome.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, Type } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 import { RouterLink } from '@angular/router';
 import { TypeWriterDirective } from '../../../../core/Directives/TypeWriterDirective';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [HeaderComponent, RouterLink, TypeWriterDirective],
+  imports: [HeaderComponent, RouterLink, TypeWriterDirective, CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
