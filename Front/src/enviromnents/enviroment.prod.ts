c7a9484172d5a84f1e6a1165742baed5b6c257d0
Oussama Zaoui <133207079+oussamazaoui@users.noreply.github.com>
2024-06-16 12:28:38 +0100

    Update enviroment.prod.ts

--- Front/src/enviromnents/enviroment.prod.ts
+++ Front/src/enviromnents/enviroment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true, // Set to true for production
     API_MESAAGING_URL: "/api/messaging/", // Update with the appropriate path
-    API_AUTH_URL: "/api/auth/", // Update with the appropriate path
+    API_AUTH_URL: "", // Update with the appropriate path
     API_NOTIFICATION_URL: "/api/notification/", // Update with the appropriate path
     API_BASE_URL: "/api/", // Update with the appropriate path
     API_CHAT_URL: "/api/chat/", // Update with the appropriate path
