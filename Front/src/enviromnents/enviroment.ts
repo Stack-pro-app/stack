dee50772b3a7b84921b195a308c91dc1e9bebf1d
Oussama Zaoui <133207079+oussamazaoui@users.noreply.github.com>
2024-06-16 02:35:31 +0100

    Update enviroment.ts

--- Front/src/enviromnents/enviroment.ts
+++ Front/src/enviromnents/enviroment.ts
@@ -1,10 +1,15 @@
 export const environment = {
   production:false,
-  API_MESAAGING_URL : "http://localhost:8090",
-  API_AUTH_URL : "http://localhost:8093",
-  API_NOTIFICATION_URL : "http://localhost:8092",
-  API_BASE_URL : "http://localhost:8094",
-  API_CHAT_URL : "http://localhost:8094",
+  // API_MESAAGING_URL : "http://localhost:8090",
+  // API_AUTH_URL : "http://localhost:8093",
+  // API_NOTIFICATION_URL : "http://localhost:8092",
+  // API_BASE_URL : "http://localhost:8094",
+  // API_CHAT_URL : "http://localhost:8094",
+  API_MESAAGING_URL: "/api/messaging/", 
+  API_AUTH_URL: "/api/auth/", 
+  API_NOTIFICATION_URL: "/api/notification/", 
+  API_BASE_URL: "/api/base/", 
+  API_CHAT_URL: "/api/chat/", 
   
 
 
