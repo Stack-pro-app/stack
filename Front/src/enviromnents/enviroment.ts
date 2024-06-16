8f243882d8f7919c27f83a5253c78ec2e5424451
Oussama Zaoui <133207079+oussamazaoui@users.noreply.github.com>
2024-06-16 11:57:43 +0100

    Update enviroment.ts

--- Front/src/enviromnents/enviroment.ts
+++ Front/src/enviromnents/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production:false,
   API_MESAAGING_URL : "http://localhost:8090",
-  API_AUTH_URL : "http://auth-service.stack-app.test:8080",
+  API_AUTH_URL : "http://localhost:8093",
   API_NOTIFICATION_URL : "http://localhost:8092",
   API_BASE_URL : "http://localhost:8094",
   API_CHAT_URL : "http://localhost:8094",
